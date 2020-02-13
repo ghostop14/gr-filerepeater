b0f28c71ad1ea2aa10d8604977c0c3e4d9ec017d
ghostop14 <ghostop14@gmail.com>
2020-02-13 09:22:44 -0500

    Cleaned up class

--- python/MetaToPair.py
+++ python/MetaToPair.py
@@ -5,47 +5,56 @@ from gnuradio import gr
 import pmt
 
 class MetaToPair(gr.sync_block):
-  def __init__(self, incomingKeyName, outgoingPairName):
-    gr.sync_block.__init__(self, name = "MetaToPair", in_sig = None, out_sig = None)
-    
-    self.incomingKeyName = str(incomingKeyName)
-    self.outgoingPairName = str(outgoingPairName)
-         
-    self.message_port_register_in(pmt.intern("inmeta"))
-    self.set_msg_handler(pmt.intern("inmeta"), self.msgHandler)   
-    self.message_port_register_out(pmt.intern("outpair"))
+    """
+    This block converts a metadata dictionary item to a pmt pair that is
+    compatible with other blocks expecting a pair in.  You can specify
+    which item in the incoming metadata to output as a pair and what
+    the pair name is.
+    """
+    def __init__(self, incomingKeyName, outgoingPairName):
+        gr.sync_block.__init__(self, name="MetaToPair", in_sig=None, out_sig=None)
 
-  def msgHandler(self, msg):
-    if not pmt.is_pair(msg):
-      print("[MetaToPair] Incoming message is not a pair.")
-      return
-      
-    meta = pmt.to_python(pmt.car(msg))
-    
-    if not type(meta) is dict:
-      print("[MetaToPair] ERROR: incoming message does not contain a dictionary.")
-      return
-      
-    if not self.incomingKeyName in meta:
-      print("[MetaToPair] ERROR: incoming message dictionary does not contain key %s" % self.incomingKeyName)
-      return
-    
-    incomingVal = meta[self.incomingKeyName]
+        self.incomingKeyName = str(incomingKeyName)
+        self.outgoingPairName = str(outgoingPairName)
 
-    newPair = None
-        
-    try:  
-    	newPair = pmt.cons(pmt.intern(self.outgoingPairName),pmt.to_pmt(incomingVal))
-    except Exception as e:
-      print("[MetaToPair] ERROR: Cannot construct new message: %s" % str(e))
-      return
-    
-    try:    
-      self.message_port_pub(pmt.intern("outpair"),newPair)
-    except Exception as e:
-      print("[MetaToPair] ERROR: Cannot send message: %s" % str(e))
-      print("Incoming dictionary (%s):" % str(type(meta)))
-      print(str(meta))    
-    
-  def stop(self):
-    return True
+        self.message_port_register_in(pmt.intern("inmeta"))
+        self.set_msg_handler(pmt.intern("inmeta"), self.msg_handler)
+        self.message_port_register_out(pmt.intern("outpair"))
+
+    def msg_handler(self, msg):
+        if not pmt.is_pair(msg):
+            gr.log.warn("Incoming message is not a pair.  Only pairs are supported.  "
+                        "No message generated.")
+            return
+
+        meta = pmt.to_python(pmt.car(msg))
+
+        if not type(meta) is dict:
+            gr.log.warn("Incoming message does not contain a dictionary.  "
+                        "No message generated.")
+            return
+
+        if not self.incomingKeyName in meta:
+            gr.log.warn("Incoming message dictionary does not contain key %s.  "
+                        "No message generated." % self.incomingKeyName)
+            return
+
+        incomingVal = meta[self.incomingKeyName]
+
+        new_pair = None
+
+        try:
+            new_pair = pmt.cons(pmt.intern(self.outgoingPairName), pmt.to_pmt(incomingVal))
+        except Exception as e:
+            gr.log.error("Cannot construct new message: %s" % str(e))
+            return
+
+        try:
+            self.message_port_pub(pmt.intern("outpair"), new_pair)
+        except Exception as e:
+            gr.log.error("Cannot send message: %s" % str(e))
+            gr.log.error("Incoming dictionary (%s):" % str(type(meta)))
+            gr.log.error(str(meta))
+
+    def stop(self):
+        return True
