7b62b92c8b610dd3642b36675ce5e90c37372337
ghostop14 <ghostop14@gmail.com>
2023-09-16 14:25:38 -0400

    Added ability to invert output

--- python/StateToBool.py
+++ python/StateToBool.py
@@ -5,13 +5,14 @@ from gnuradio import gr
 import pmt
 
 class StateToBool(gr.sync_block):
-  def __init__(self):
+  def __init__(self,  invert = False):
     gr.sync_block.__init__(self, name = "State Message To Boolean", in_sig = None, out_sig = None)
     
     self.message_port_register_in(pmt.intern("state"))
     self.message_port_register_out(pmt.intern("bool"))
     self.set_msg_handler(pmt.intern("state"), self.stateHandler)   
-
+    self.invert = invert
+    
   def stateHandler(self, pdu):
     try:    
       newState = pmt.to_python(pmt.cdr(pdu))
@@ -21,8 +22,11 @@ class StateToBool(gr.sync_block):
       else:
         boolState = False
       
+      if self.invert:
+          boolState = not boolState
+          
       self.message_port_pub(pmt.intern("bool"),pmt.to_pmt(boolState))
       
     except Exception as e:
       print("[State to Bool] Error with state message: %s" % str(e))
-      print(str(meta))    
+      print(str(newState))    
